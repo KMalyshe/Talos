1b1d3bfcc33e1f629279656e989af6132f608dfa
KMalyshe <kyryl.malyshev@gmail.com>
2024-02-03 19:10:16 +0100

    aaaaa

--- Talos/Modules/FirstModule.cs
+++ Talos/Modules/FirstModule.cs
@@ -2,5 +2,18 @@ using Discord.Commands;
 
 public class InfoModule : ModuleBase<SocketCommandContext>
 {
-    
+    [Command("say")]
+	[Summary("Echoes a message.")]
+    // say command to say something
+	public Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+		=> ReplyAsync(echo);
+    // square a number
+	[Command("square")]
+	[Summary("Squares a number.")]
+	public async Task SquareAsync(
+		[Summary("The number to square.")] 
+		int num)
+	{
+		await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
+	}
 }
