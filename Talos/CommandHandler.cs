cb75a848866eac5a72027a091e7472f5167b9920
KMalyshe <kyryl.malyshev@gmail.com>
2024-02-13 16:10:14 +0100

    sadge

--- Talos/CommandHandler.cs
+++ Talos/CommandHandler.cs
@@ -3,23 +3,25 @@ using Discord.Commands;
 using System.Reflection;
 public class CommandHandler
 {
-    private readonly DiscordSocketClient client;
-    private readonly CommandService commands;
+    private readonly DiscordSocketClient _client;
+    private readonly CommandService _commands;
 
-    // Retrieve client and CommandService instance
-    public CommandHandler(DiscordSocketClient client, CommandService command)
+    // Retrieve client and CommandService instance via ctor
+    public CommandHandler(DiscordSocketClient client, CommandService commands)
     {
-        this.commands = command;
-        this.client = client;
+        _commands = commands;
+        _client = client;
     }
     
     public async Task InstallCommandsAsync()
     {
-        // Hook the MessageReceived event into command handler
-        client.MessageReceived += HandleCommandAsync;
+        // Hook the MessageReceived event into our command handler
+        _client.MessageReceived += HandleCommandAsync;
 
-        await commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
+        await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), 
+                                        services: null);
     }
+
     private async Task HandleCommandAsync(SocketMessage messageParam)
     {
         // Don't process the command if it was a system message
@@ -30,16 +32,19 @@ public class CommandHandler
         int argPos = 0;
 
         // Determine if the message is a command based on the prefix and make sure no bots trigger commands
-        if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos)) || message.Author.IsBot)
-        {
+        if (!(message.HasCharPrefix('!', ref argPos) || 
+            message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+            message.Author.IsBot)
             return;
-        }
 
         // Create a WebSocket-based command context based on the message
-        var context = new SocketCommandContext(client, message);
+        var context = new SocketCommandContext(_client, message);
 
         // Execute the command with the command context we just
         // created, along with the service provider for precondition checks.
-        await commands.ExecuteAsync(context: context, argPos: argPos, services: null);
+        await _commands.ExecuteAsync(
+            context: context, 
+            argPos: argPos,
+            services: null);
     }
 }
--- Talos/Modules/FirstModule.cs
+++ Talos/Modules/FirstModule.cs
@@ -1,35 +1,42 @@
 using Discord.Commands;
 using Discord.WebSocket;
 
+// Create a module with no prefix
 public class InfoModule : ModuleBase<SocketCommandContext>
 {
-    [Command("say")]
+	// ~say hello world -> hello world
+	[Command("say")]
 	[Summary("Echoes a message.")]
-    // say command to say something
 	public Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
 		=> ReplyAsync(echo);
-    // square a number
+		
+	// ReplyAsync is a method on ModuleBase 
+}
+
+// Create a module with the 'sample' prefix
+[Group("sample")]
+public class SampleModule : ModuleBase<SocketCommandContext>
+{
+	// ~sample square 20 -> 400
 	[Command("square")]
 	[Summary("Squares a number.")]
 	public async Task SquareAsync(
 		[Summary("The number to square.")] 
 		int num)
 	{
+		// We can also access the channel from the Command Context.
 		await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
 	}
-	// divide a number by 2
-	[Command("half")]
-	[Summary("Divides a number by 2.")]
-	public async Task HalfAsync(
-		[Summary("The number to square.")] 
-		int num)
-	{
-		await Context.Channel.SendMessageAsync($"{num}/2 = " + num/2);
-	}
 
+	// ~sample userinfo --> foxbot#0282
+	// ~sample userinfo @Khionu --> Khionu#8708
+	// ~sample userinfo Khionu#8708 --> Khionu#8708
+	// ~sample userinfo Khionu --> Khionu#8708
+	// ~sample userinfo 96642168176807936 --> Khionu#8708
+	// ~sample whois 96642168176807936 --> Khionu#8708
 	[Command("userinfo")]
 	[Summary
-	("Returns user info")]
+	("Returns info about the current user, or the user parameter, if one passed.")]
 	[Alias("user", "whois")]
 	public async Task UserInfoAsync(
 		[Summary("The (optional) user to get info from")]
