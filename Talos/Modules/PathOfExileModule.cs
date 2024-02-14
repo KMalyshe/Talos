dd27ec6037f331831238def4676b9b258bf4e800
KMalyshe <kyryl.malyshev@gmail.com>
2024-02-14 15:24:08 +0100

    bugfix for "of" and "the" names

--- Talos/Modules/PathOfExileModule.cs
+++ Talos/Modules/PathOfExileModule.cs
@@ -9,13 +9,18 @@ namespace TalosBot.Modules
 
         public async Task PoeSearch(params string[] search)
         {
-            // Capitalize the first letter of every word for the wiki
+            // Capitalize the 
             var reply = "";
             foreach (string word in search)
             {
                 if (word.Length == 1)
                 {
                     reply += word.ToUpper() + "_";
+                    continue;
+                }
+                if (((word.ToLower() == "of") || (word.ToLower() == "the")) && (word != search[0]) && (word != search.Last()))
+                {
+                    reply += word.ToLower() + "_";
                     continue;
                 }
                 reply += char.ToUpper(word.First()) + word.Substring(1).ToLower() + "_";
