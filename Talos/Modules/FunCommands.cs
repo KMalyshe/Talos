9d0bea034378755207c1d67aacf2344ebcc6a291
KMalyshe <kyryl.malyshev@gmail.com>
2024-02-20 15:53:59 +0100

    finished !fish and !fishcollection, SQL learned (kind of)

--- Talos/Modules/FunCommands.cs
+++ Talos/Modules/FunCommands.cs
@@ -1,5 +1,7 @@
 using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Data.Sqlite;
 
 namespace TalosBot.Modules
 {
@@ -34,6 +36,103 @@ namespace TalosBot.Modules
             embed.AddField("The ball's infinite wisdom proclaims the answer.", reply)
             .WithCurrentTimestamp();
             await ReplyAsync(embed: embed.Build());
+        }
+
+        [Command ("fish")]
+
+        public async Task fish(SocketUser user = null)
+        {
+            Random rng = new Random();
+            var fishDict = new Dictionary<int, string>(){
+                {12, "T5FISH"},
+                {6, "T4FISH"},
+                {3, "T3FISH"},
+                {1, "T2FISH"},
+                {0, "T1FISH"}
+            };
+            var fish = rng.Next(20);
+            var fishcaught = "";
+            foreach (int key in fishDict.Keys)
+            {
+                if (fish >= key)
+                {
+                    fishcaught = fishDict[key];
+                    break;
+                }
+            }
+            var embed = new EmbedBuilder();
+            var fishParsing = new Dictionary<string, string>(){
+                {"T1FISH", "... 🐠. What an incredible catch! You should hit the lotto today."},
+                {"T2FISH", "... 🦐. It's that shrimple."},
+                {"T3FISH", "... 🐟. At least it's a fish."},
+                {"T4FISH", "... 🔋. It's still charged, somehow."},
+                {"T5FISH", "... 👢. Better luck next time, cowboy."}
+            };
+            var whichFish = fishParsing[fishcaught];
+            embed.AddField("You cast your mighty rod into the Discordian depths and catch...", whichFish).WithColor(Color.Blue);
+            await ReplyAsync(embed: embed.Build());
+            using (var connection = new SqliteConnection(@"Data Source=C:\TalosFiles\SQL\fish.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                if (user == null) user = Context.User;
+                command.CommandText = 
+                @"
+                INSERT OR IGNORE INTO UserCollection(USERID, T1FISH, T2FISH, T3FISH, T4FISH, T5FISH) VALUES($name, 0, 0, 0, 0, 0)
+                ";
+                command.Parameters.AddWithValue("$name", user.Username);
+                command.ExecuteNonQuery();
+
+                command.CommandText =
+                @$"
+                UPDATE UserCollection
+                SET {fishcaught} = {fishcaught} + 1
+                WHERE USERID = @name
+                ";
+                command.Parameters.AddWithValue("@name", user.Username);
+                command.ExecuteNonQuery();
+
+            }
+        }
+
+        [Command ("fishcollection")]
+
+        public async Task fishCollection(SocketUser user = null)
+        {
+            using (var connection = new SqliteConnection(@"Data Source=C:\TalosFiles\SQL\fish.db"))
+            {
+                List<int> fishAmounts = new List<int>();
+
+                connection.Open();
+                var command = connection.CreateCommand();
+                var userinfo = user ?? Context.User;
+                command.CommandText = @"SELECT T1FISH, T2FISH, T3FISH, T4FISH, T5FISH FROM UserCollection WHERE USERID = $name;";
+                command.Parameters.AddWithValue("$name", userinfo.Username);
+                SqliteDataReader reader = command.ExecuteReader();
+                if (reader.Read()) {
+                    fishAmounts.Add(Convert.ToInt32(reader[0]));
+                    fishAmounts.Add(Convert.ToInt32(reader[1]));
+                    fishAmounts.Add(Convert.ToInt32(reader[2]));
+                    fishAmounts.Add(Convert.ToInt32(reader[3]));
+                    fishAmounts.Add(Convert.ToInt32(reader[4]));
+                }
+                var embed = new EmbedBuilder();
+                embed.AddField(userinfo.Username + "'s", "Marvelous Fishing Collection");
+                var fishDict = new Dictionary<int, string>()
+                {
+                {0, "🐠 \nThe most tropical of fish. \n**"},
+                {1, "🦐 \nIt could not be more shrimple. \n**"},
+                {2, "🐟 \nThe kind you'd find at the market. \n**"},
+                {3, "🔋 \nYou can charge something, I guess? \n**"},
+                {4, "👢 \nEventually, you'll find the right size. \n**"}
+                };
+                for (int i = 0; i<5; i++)
+                {
+                    embed.AddField("Tier " + i + " Fish:", fishDict[i] + fishAmounts[i].ToString() + "**");
+                }
+                embed.AddField("Total Fish Accrued:", "**" + fishAmounts.Sum() + "**").WithColor(Color.Blue);
+                await ReplyAsync(embed: embed.Build());
+            }
         }
     }
 }
